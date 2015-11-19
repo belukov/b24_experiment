f4f8d2c0360c50e70344aa6e3ee1035bee45e335
belukov at home <belukov@gmail.com>
2015-11-20 01:11:38 +0400

    next

--- b24.js
+++ b24.js
@@ -1,1 +1,19 @@
-console.log("loaded script!!!", BX24);
+console.log("loaded script!!!");
+
+
+BX24.init(function()
+{
+	BX24.callMethod(
+		"entity.get", 
+		{},
+		function(result)
+		{
+			if(result.error()) {
+				console.error(result.error());
+			} else {
+				console.info("Список созданных хранилищ:", result.data());
+			}
+		}
+	);
+});
+
