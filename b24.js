8a4a2b0a7f7dacac77c08684f89cee54b0eec6f9
belukov at home <belukov@gmail.com>
2015-11-20 01:15:32 +0400

    next

--- b24.js
+++ b24.js
@@ -5,13 +5,25 @@ BX24.init(function()
 {
 	BX24.callMethod(
 		"entity.get", 
-		{},
+		{ENTITY: 'taskflow'},
 		function(result)
 		{
 			if(result.error()) {
 				console.error(result.error());
 			} else {
-				console.info("Список созданных хранилищ:", result.data());
+				list = result.data();
+				console.info("Список созданных хранилищ:", list);
+				if(!list){
+					console.log("no entity found. add it..")
+					BX24.callMethod(
+						'entity.add', 
+						{
+							'ENTITY': 'taskflow', 
+							'NAME': 'TaskFlow',
+							'ACCESS': {U1:'W',AU:'W'}
+						}
+					);
+				}
 			}
 		}
 	);
